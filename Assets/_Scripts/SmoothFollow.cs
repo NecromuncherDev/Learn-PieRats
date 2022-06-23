62959add1c5637f78e1960e17abe6ff03bde28d5
NecromuncherDev <Oded56@gmail.com>
2022-06-23 23:58:50 +0300

    Camera smooth follow player

---
+++ Assets/_Scripts/SmoothFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField, Range(0f,5f)] private float dampening;
+    
+    private Vector2 targetPos, transformPos, newPos;
+    private Vector2 velocity = Vector2.zero;
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            targetPos = target.position;
+            transformPos = transform.position;
+
+            newPos = Vector2.SmoothDamp(transformPos, targetPos, ref velocity, dampening);
+            transform.Translate(newPos - transformPos);
+        }
+    }
+}
